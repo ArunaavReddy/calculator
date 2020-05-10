7dd4b3744a8833ee066240d91105ab77b2d82950
Arunaav <arunavreddy007@gmail.com>
2020-05-10 18:56:44 +0530

    maven project

--- src/test/java/com/calculator/calculator/AppTest.java
+++ src/test/java/com/calculator/calculator/AppTest.java
@@ -1,38 +1,60 @@
 package com.calculator.calculator;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.*;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class Calculator {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public static void main(String[] args){
+		int no1,no2,operation;
+		Scanner reader = new Scanner(System.in);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		while(true){
+			System.out.println("1.Addition\n2.Subtraction\n3.Muliplication\n4.Division\n5.Exit");
+			System.out.print("Choose the operation: ");
+			operation = reader.nextInt();
+			if(operation==5){System.out.println("Have a Nice Day :)");break;}
+			else{
+							System.out.print("Enter the numbers with a space: ");
+							no1 = reader.nextInt();
+							no2 = reader.nextInt();
+							switch(operation){
+								case 1:
+									System.out.println("Sum of the numbers is "+add(no1,no2));
+									break;
+								case 2:
+									System.out.println("Difference of the numbers is "+subtract(no1,no2));
+									break;
+								case 3:
+									System.out.println("Product of the numbers is "+multiply(no1,no2));
+									break;
+								case 4:
+									System.out.println("cofficient of the numbers is "+divide(no1,no2));
+									break;
+							}
+
+						}
+		}
+	}
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public static long multiply(int a, int b) {
+		return a * b;
+	}
+
+	public static double divide(int a, int b) {
+		double result;
+		if (b == 0) {
+			throw new IllegalArgumentException("Divisor cannot divide by zero");
+		} else {
+			result = Double.valueOf(a)/Double.valueOf(b);
+		}
+		return result;
+	}
 }
